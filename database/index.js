cf5f322257c1175ce8f58096edfa092ada973aa4
diego1891 <diegobronc1508@gmail.com>
2023-10-17 20:42:57 -0300

    Req Funcionales Obligatorios terminados

---
+++ database/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+export const dbConnectionMongo = async () => {
+  try {
+    await mongoose.connect("mongodb+srv://diegobronc1508:mongodbnosql@diegobronc.gmneksw.mongodb.net/?retryWrites=true&w=majority", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (error) {
+    console.log(error);
+    throw new Error ("Se rompió")
+  }
+
+};
+
